975541a372b9e14acd8731981c45e5c79f01840e
kurai-sx <surajnagre@gmail.com>
2025-09-15 18:01:33 +0530

    initial commit

---
+++ scripts/gpt-neo_model.py
@@ -0,0 +1,11 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "EleutherAI/gpt-neo-1.3B"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    device_map="auto",
+    torch_dtype="float16"  # good for Apple MPS
+)
